7ce90574838259f07d0abb824c19525c5c44f857
Alex Velea <veleandu@yahoo.com>
2015-06-12 12:42:50 +0300

    automaton

--- osx.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cerr << "123OSX is here!\n";
-    return 0;
-}
--- ubuntu.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    cerr << "OSX WAS HERE!\n";
-    cerr << "Ermafgerd\n";
-    cerr << "Ubuntu commit <3";
-    return 0;
-}
